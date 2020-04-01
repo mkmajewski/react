98c1b550716b972df92c67ff90182e7a7f51581a
Marcin Majewski <marcin.majewski@prime-force.com>
2020-04-01 18:43:45 +0200

    01

---
+++ 01/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Company from './Company/Company';
+
+function App() {
+  return (
+    <Company name="Whizzarts"/>
+  );
+}
+
+export default App;
---
+++ 01/src/Company/Company.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Company = props => {
+    return (
+        <h1>Welcome in {props.name}!</h1>
+    )
+}
+
+export default Company;
